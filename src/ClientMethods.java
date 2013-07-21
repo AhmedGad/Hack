28b6f818925d7e1bbb46898026305187e466a61e
AhmedGad <ahmed.mu.gad@gmail.com>
2013-07-21 06:51:42 +0100

    server works

---
+++ src/ClientMethods.java
@@ -0,0 +1,196 @@
+import java.awt.Point;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class ClientMethods {
+
+	public static HttpURLConnection initConnection() {
+		HttpURLConnection httpConn = null;
+		try {
+			URL url = new URL("http://hackserver.herokuapp.com");
+			httpConn = (HttpURLConnection) url.openConnection();
+			httpConn.setDoOutput(true);
+			httpConn.setRequestMethod("POST");
+			httpConn.connect();
+		} catch (Exception e) {
+		}
+		return httpConn;
+	}
+
+	// return playerId
+	static public int newPlayer(int i, int j, String name) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("new " + i + " " + j + " " + name).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		return new Integer(tempstr.trim());
+	}
+
+	// doesn't return any fucking thing
+	static public void move(int gameIndex, int indexInGame, int dir)
+			throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("move " + gameIndex + " " + indexInGame + " " + dir)
+				.getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+	}
+
+	// return game Index
+	static public int host(int id) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("host " + id).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		return new Integer(tempstr.trim());
+	}
+
+	// return Index in game
+	static public int join(int gameIndex, int id) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("join " + gameIndex + " " + id).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		return new Integer(tempstr.trim());
+	}
+
+	// return list of free games
+	static public ArrayList<Integer> getFreeGames() throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("getFreeGames").getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		StringTokenizer tok = new StringTokenizer(tempstr);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while (tok.hasMoreElements())
+			list.add(new Integer(tok.nextToken()));
+		return list;
+	}
+
+	// also doesn't return a fucking thing
+	static public void start(int gameIndex) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("start " + gameIndex).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+	}
+
+	// same
+	static public void stop(int gameIndex, int indexInGame) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("stop " + gameIndex).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+	}
+
+	// mohemma fash5 - return list of player's i and j
+	static public ArrayList<Point> gameState(int gameIndex) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("gameState " + gameIndex).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		StringTokenizer tok = new StringTokenizer(tempstr);
+		ArrayList<Point> lst = new ArrayList<Point>();
+		while (tok.hasMoreElements())
+			lst.add(new Point(new Integer(tok.nextToken()), new Integer(tok
+					.nextToken())));
+		return lst;
+	}
+
+	// return if this game has started
+	public static boolean isRunning(int gameIndex) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("isRunning " + gameIndex).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		return tempstr.equals("0") ? false : true;
+	}
+
+	// return name list
+	public static ArrayList<String> getNameList(int gameIndex) throws Exception {
+		HttpURLConnection httpConn = initConnection();
+		OutputStream os = httpConn.getOutputStream();
+		os.write(("gameState " + gameIndex).getBytes());
+		os.close();
+		InputStream in = httpConn.getInputStream();
+		byte[] buffer = new byte[1000];
+		int read;
+		String tempstr = "";
+		while ((read = in.read(buffer)) != -1)
+			tempstr += new String(buffer, 0, read);
+		in.close();
+		StringTokenizer tok = new StringTokenizer(tempstr);
+		ArrayList<String> lst = new ArrayList<String>();
+		while (tok.hasMoreElements())
+			lst.add(tok.nextToken());
+		return lst;
+	}
+
+}
--- src/MazeUI.java
+++ src/MazeUI.java
@@ -2,8 +2,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
+import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,16 +12,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.Timer;
-import javax.swing.border.EmptyBorder;
 
-public class MazeUI extends JFrame implements KeyListener {
+public class MazeUI extends JFrame implements KeyListener, ActionListener {
 
 	/**
 	 * 
@@ -51,29 +47,41 @@ public class MazeUI extends JFrame implements KeyListener {
 
 	}
 
-	static ActionListener taskPerformer = new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent evt) {
+	static int di = 2, dj = 2;
+
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		for (player pl : players) {
+			if (pl.curri != pl.disti) {
+				if (pl.curri < pl.disti)
+					pl.curri += di;
+				else if (pl.curri > pl.distj)
+					pl.curri -= di;
+
+				if (Math.abs(pl.curri - pl.disti) < di)
+					pl.curri = pl.disti;
+			}
+
+			if (pl.currj != pl.distj) {
+				if (pl.currj < pl.distj)
+					pl.currj += dj;
+				else if (pl.currj > pl.distj)
+					pl.currj -= dj;
+
+				if (Math.abs(pl.currj - pl.distj) < dj)
+					pl.currj = pl.distj;
+			}
 		}
-	};
+		repaint();
+	}
 
 	static ActionListener serv_taskPerformer = new ActionListener() {
-		private int gameIndex = -1;
-		private int id = -1;
-		private int indexInGame = -1;
 
 		@Override
 		public void actionPerformed(ActionEvent evt) {
-			URL url;
 			try {
-				url = new URL("http://hackserver.herokuapp.com");
-				HttpURLConnection httpConn = (HttpURLConnection) url
-						.openConnection();
-				httpConn.setDoOutput(true);
-				httpConn.setRequestMethod("POST");
-				httpConn.connect();
+				HttpURLConnection httpConn = ClientMethods.initConnection();
 				OutputStream os = httpConn.getOutputStream();
-
 				if (!joined) {
 					os.write("getFreeGames".getBytes());
 					os.close();
@@ -118,7 +126,7 @@ public class MazeUI extends JFrame implements KeyListener {
 	};
 
 	private static Timer serv_timer = new Timer(100, serv_taskPerformer);
-	private static Timer timer = new Timer(50, taskPerformer);
+	private Timer timer = new Timer(50, this);
 
 	/**
 	 * Launch the application.
@@ -137,25 +145,42 @@ public class MazeUI extends JFrame implements KeyListener {
 		});
 	}
 
+	private static int gameIndex, indexInGame;;
+
 	/**
 	 * Create the frame.
 	 * 
 	 * @throws IOException
 	 */
-	public MazeUI() throws IOException {
+	public MazeUI() throws Exception {
+		int id = ClientMethods.newPlayer(5, 5, "test");
+		gameIndex = ClientMethods.host(id);
+		joined = true;
+		ClientMethods.start(gameIndex);
+		ArrayList<Point> gameState = ClientMethods.gameState(gameIndex);
+		indexInGame = 0;
+		for (int i = 0; i < gameState.size(); i++)
+			players.add(new player(Color.red, 5 * blockSizeHeight,
+					5 * blockSizeWidth, 5 * blockSizeHeight, 5 * blockSizeWidth));
+
+		serv_timer.start();
+		timer.start();
+		// make new player and
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, screenSize.width, screenSize.height);
 		FullScreenFrame();
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setSize(dim);
 
-		mazeWidth = 200;
-		mazeHeight = 200;
+		mazeWidth = 200 + 1;
+		mazeHeight = 200 + 1;
 
-//		System.out.println(getHeight() + "   " + dim.height);
+		// System.out.println(getHeight() + "   " + dim.height);
 		blockSizeWidth = (dim.width) / mazeWidth;
 		blockSizeHeight = (dim.height) / mazeHeight;
-		MazeGenerator generator = new MazeGenerator(mazeWidth, mazeHeight);
+		MazeGenerator generator = new MazeGenerator(mazeWidth - 1,
+				mazeHeight - 1);
 		generator.generate();
 		mazeMap = generator.getMap();
 		// drawMap();
@@ -164,20 +189,20 @@ public class MazeUI extends JFrame implements KeyListener {
 
 	private void FullScreenFrame() {
 
-		 GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment()
-		 .getDefaultScreenDevice();
-		
-		 if (gd.isFullScreenSupported()) {
-		 setUndecorated(true);
-		 gd.setFullScreenWindow(this);
-		 } else {
-		 System.err.println("Full screen not supported");
-		 setSize(100, 100); // just something to let you see the window
-		 setVisible(true);
-		 }
-//		System.err.println("Full screen not supported");
-//		setSize(100, 100); // just something to let you see the window
-//		setVisible(true);
+		// GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment()
+		// .getDefaultScreenDevice();
+		//
+		// if (gd.isFullScreenSupported()) {
+		// setUndecorated(true);
+		// gd.setFullScreenWindow(this);
+		// } else {
+		// System.err.println("Full screen not supported");
+		// setSize(100, 100); // just something to let you see the window
+		// setVisible(true);
+		// }
+		System.err.println("Full screen not supported");
+		setSize(100, 100); // just something to let you see the window
+		setVisible(true);
 	}
 
 	@Override
@@ -191,6 +216,11 @@ public class MazeUI extends JFrame implements KeyListener {
 							blockSizeWidth, blockSizeHeight);
 				}
 			}
+		}
+		for (player pl : players) {
+			g.setColor(pl.color);
+			g.fillOval(pl.curri,pl.currj, blockSizeWidth * 3,
+					blockSizeHeight * 3);
 		}
 	}
 
@@ -216,12 +246,34 @@ public class MazeUI extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent k) {
-		if (k.getKeyCode() == KeyEvent.VK_ESCAPE) {
-			// GraphicsDevice gd = GraphicsEnvironment
-			// .getLocalGraphicsEnvironment().getDefaultScreenDevice();
-			// gd.setFullScreenWindow(null);
+		int dir = -1;
+		switch (k.getKeyCode()) {
+		case KeyEvent.VK_ESCAPE:
 			System.exit(0);
+			break;
+		case KeyEvent.VK_UP:
+			dir = 0;
+			break;
+		case KeyEvent.VK_DOWN:
+			dir = 1;
+			break;
+		case KeyEvent.VK_RIGHT:
+			dir = 2;
+			break;
+		case KeyEvent.VK_LEFT:
+			dir = 3;
+			break;
+
+		default:
+			break;
 		}
+		if (dir > -1)
+			try {
+				ClientMethods.move(gameIndex, indexInGame, dir);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 	@Override
